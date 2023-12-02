cbb2ef187ac2ebc4a6ae96a952b2c8d923ede1c3
Moritz Mühlbeyer <moritz.muehlbeyer@gmx.de>
2023-12-02 18:45:25 +0100

    Code für alle Sensoren existent (funktional?)

--- Software.ino
+++ Software.ino
@@ -1,6 +1,12 @@
 // Software.ino
 
+#include <tuple>
+#include <iostream>
 #include <MCP3XXX.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "DFRobot_EC10.h"
+#include <EEPROM.h>
 #include "DHT.h"
 #include "Sensor.h"
 
@@ -10,52 +16,69 @@
 //Pins on ADC
 const int PH_PIN = 0;
 const int EC_PIN = 1;
-const int WATERTEMP_PIN = 3;
-const int WATERFLOW_PIN = 4;
 
 //Digital Pins
 const int WATERLEVEL_PIN = 0;
+const int WATERTEMP_PIN = 1;
 
 const int PUMP_REL_PIN = 1;
 const int FERT_PUMP_PIN = 2;
 const int PH_DOWN_PUMP_PIN = 3;
 
-DHT dht(TEMP_HUM_PIN, TEMP_HUM_TYPE);
+DHT tempHumSensor(TEMP_HUM_PIN, TEMP_HUM_TYPE);
 Sensor waterlevelSensor(WATERLEVEL_PIN, 'D');
+Sensor pHSensor(PH_PIN, 'A');
+DFRobot_EC10 ec;
+Sensor ecSensor(EC_PIN, 'A');
+OneWire oneWire(WATERTEMP_PIN);
+DallasTemperature watertempSensor(&oneWire);
 
 float air_temp;
 float humidity;
 float pH_value;
+float ec_voltage;
 float ec_value;
 int waterlevel;
-float watertemp;
-float waterflow;
+float watertemp = 20;
 
 void setup(){
   Serial.begin(9600);
-  dht.begin();
+  tempHumSensor.begin();
+  ec.begin();
+  watertempSensor.begin();
 }
 
 void loop(){
   
   measureData();
 
-  Serial.println("..................................................");
-  Serial.print("Waterlevel: "); Serial.println(waterlevel);
   delay(2000);
 }
 
-void getTempAndHumidity(int pin){
-  dht.read(TEMP_HUM_PIN);
-  air_temp = dht.readTemperature();
-  humidity = dht.readHumidity();
-  Serial.print("Temperature: "); Serial.print(air_temp); Serial.print(" C     Humidity: "); Serial.print(humidity); Serial.println("%");
-  Serial.println();
-}
-
 void measureData(){
-  //getTempAndHumidity(TEMP_HUM_PIN);
-  waterlevel = waterlevelSensor.getSensorData();
+  static unsigned long timepoint = millis();
+  if(millis()-timepoint>2000U){  //time interval: 2s
+    timepoint = millis();
+    
+    /*
+    air_temp = tempHumSensor.readTemperature();
+    humidity = tempHumSensor.readHumidity();
+    Serial.print("Temperature: "); Serial.print(air_temp); Serial.print(" C     Humidity: "); Serial.print(humidity); Serial.println("%"); Serial.println(); //Output Temp & Humidity
+    */
+
+    watertempSensor.requestTemperatures();
+    watertemp = watertempSensor.getTempCByIndex(0);
+
+    float pH_Voltage = pHSensor.getSensorData() / 1024 * 3.3; 
+    
+    ec_voltage = ecSensor.getSensorData()/1024.0*3300;   // read the voltage
+    ec_value =  ec.readEC(ec_voltage, watertemp);  // convert voltage to EC with temperature compensation
+    Serial.print("temperature:"); Serial.print(watertemp,1); Serial.print("^C  EC:"); Serial.print(ec_value,2); Serial.println("ms/cm");
+
+    waterlevel = waterlevelSensor.getSensorData();
+    Serial.print("Waterlevel: "); Serial.println(waterlevel); Serial.println();
+  }
+  ec.calibration(ec_voltage, watertemp);
 }
 
 
